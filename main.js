3040cf1bf31ae5d0ea7cc5351b5311d5b848f705
rocioacosta <rociiobelen_2273@hotmail.com>
2023-11-12 01:01:40 -0300

    condicional, bucle y funcion

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
 
 
 alert("Bienvenido a BIG envios");
-alert("Servicios - paqueteria, mensajeria");
+alert("Servicio de encomiendas por paqueteria y seguimiento");
 
-
-let correo = prompt("ingrese que tipo de servicio requiere");
-if(correo = "mensajeria", "paqueteria"){
+let servicios = "paquete"; "seguimiento";
+let correo = prompt("Requiere enviar un paquete o verificar seguimiento?");
+if(correo !== servicios){
+    alert("El servicio que busca no corresponde")
 }else{
-    alert("servicio no valido");                                        
+    alert("vamos alla!")
 }
 
-let destino = prompt("ingrese el destino");
-switch(destino){
-    case "la plata":
-        alert("llegara en 24hs");
-        break;
-    case "caba":
-        alert ("el envio demora entre 24 - 48hs");
-        break;
-    case "gba":
-        alert("demora entre 48 - 96hs");
-        break;
-    default:
-        alert("ingrese destino valido");
-        break;
-} let enviar = confirm("desea continuar con el envio?");
 
 
-
-let peso = parseInt(prompt("ingrese el peso aproximado del envio"));
-for(let peso = 2; peso <= 5; peso++ ){
-    if(peso === 2){
-        alert("su pedido tiene un costo de $1875.82");
-    let d = confirm("desea continuar con el envio?");
-    if(d==true){
-        alert("todo listo, muchas gracias");
-    }else{
-        alert("Adios!");
-    }
-    }
+function peso(){
+    let kg = 970;
+    let peso = prompt("indique el peso aproximado de su encomienda");
+    for (let i = 0; i = peso;){
+    i *= kg;
+    alert("el costo de su envio es de " + i);
+    break;
+   }   
+   let e = confirm("desea continuar con el envio?");
+}
+function Envio(){
+    let env = prompt("ingrese destino del paquete")
+switch(env){
+ case "la plata":
+     alert("su encomienda llegara en 24hs");
+     break;
+ case "caba":
+     alert("su encomienda llegara entre 48-72hs");
+     break;
+ case "gba":
+     alert("su encomienda llegara entre 72-96hs");
+     break;
+ default:
+     alert("fuera de alcance de envio, disculpe!");
+     break;
+}
+}
+if(correo == servicios){
+    peso();
+    Envio();
 }
