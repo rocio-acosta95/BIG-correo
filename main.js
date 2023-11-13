a729e8d83e9e342ddae2a4d5acfd3e9c606de406
rocioacosta <rociiobelen_2273@hotmail.com>
2023-11-13 09:49:34 -0300

    cambios en la funcion peso

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 
 
 alert("Bienvenido a BIG envios");
-alert("Servicio de encomiendas por paqueteria y seguimiento");
+alert("Servicio de envios por paqueteria");
 
-let servicios = "paquete"; "seguimiento";
-let correo = prompt("Requiere enviar un paquete o verificar seguimiento?");
+const servicios = "envio";
+let correo = prompt("indique el servicio que requiere");
 if(correo !== servicios){
     alert("El servicio que busca no corresponde")
 }else{
-    correo == servicios
     peso();
     Envio();
 }
+
 function peso(){
-    let kg = 970;
-    let peso = parseFloat(prompt("ingrese el peso del producto a enviar"));
-    for (let i = 0; i = peso;){
-    i *= kg;
-    alert("el costo de su envio es de " + i);
-    break;
-   }   
-   let e = confirm("desea continuar con el envio?");
+   const precioPorKg = 970;
+    let precioFinal;
+    do{
+        precioFinal = parseFloat(prompt("ingrese el peso de lo que quiere enviar"))
+        precioFinal *= precioPorKg
+    }while(isNaN(precioFinal));
+    alert("el costo de su envio es de " + precioFinal);
 }
+
 function Envio(){
    alert("Nuestro alcance de envio es (La plata, Caba, Gba)")
    let env =  prompt("ingrese localidad a la que requiere hacer el envio");
